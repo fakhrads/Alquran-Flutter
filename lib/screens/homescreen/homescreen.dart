18da52cbd545e7106926cb128f92b69ceb89242e
fakhrads <fakhriadisaputra15@gmail.com>
2022-04-25 11:12:06 +0700

    First Initialize

---
+++ lib/screens/homescreen/homescreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Al-Quran Indonesia")),
+      body: Container(child: Text("Quran")),
+    );
+  }
+}
